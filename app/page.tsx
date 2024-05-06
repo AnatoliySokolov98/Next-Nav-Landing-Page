9d9954a4281aedfbefd94286df12a47868ee18de
AnatoliySokolov98 <anatoliysokolov98@gmail.com>
2024-05-05 23:54:43 -0700

    updated users

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
               User friendly VSCode extension
             </h3>
             <h3 className={title({ color: "green", size: "sm" })}>
-              (10k+ downloads)
+              (15k+ downloads)
             </h3>
           </div>
           <div className="flex gap-3 justify-center mt-10">
